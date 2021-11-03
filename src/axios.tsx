ab5bb8fccc1eddb8be6927c49e86d01bd879699c
VictorSandoval <53907515+VictorSandoval-VG@users.noreply.github.com>
2021-11-02 20:12:27 -0500

    update link backend heroku

--- src/axios.tsx
+++ src/axios.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL: 'http://localhost:9095'
+    baseURL: 'https://ibm-translate.herokuapp.com/'
 })
 
 export default instance
